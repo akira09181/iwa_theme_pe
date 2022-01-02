253f9f95ce50d0d06c50174cd8ad445db5351898
Akira <freelance0918@gmail.com>
2022-01-02 17:54:45 +0900

    third commit

--- gmailservice/models.py
+++ gmailservice/models.py
@@ -6,7 +6,8 @@ class Users(models.Model):
     id = models.CharField(max_length=30,verbose_name='Gmailアドレス',primary_key=True)
 
 class Mails(models.Model):
-    name = models.CharField(default = "",max_length=100)
-    date = models.CharField(default = "",max_length=100)
+    no = models.IntegerField(default=0,primary_key=True)
+    date = models.CharField(default = "",max_length=100,null=True)
     domain = models.CharField(default = "",max_length=100)
-    title = models.CharField(primary_key=True,default = "",max_length=200)
+    title = models.CharField(default = "",max_length=200)
+    now = models.CharField(default="",max_length=100)
--- gmailservice/views.py
+++ gmailservice/views.py
@@ -7,6 +7,7 @@ from email.parser import BytesParser
 from email import policy
 from .models import Mails
 import datetime
+from pprint import pprint
 # Create your views here.
 def index(request):
     
@@ -18,10 +19,13 @@ def login(request):
     pas = request.GET['password']
     gmail = imaplib.IMAP4_SSL("imap.gmail.com", '993')
     gmail.login(id, pas)
-    gmail.select()
+    c = gmail.list()
+    pprint(c[1])
+    gmail.select('theme')
     dt = datetime.datetime.today()
     today = str(dt.year)+'-'+str(dt.month)+'-'+str(dt.day)
-   
+    d = Mails.objects.all()
+    d.delete()
     
     head, data = gmail.search(None, 'ALL')
     datas = data[0].split()
@@ -29,39 +33,70 @@ def login(request):
     if (len(datas)-fetch_num)<0:
         fetch_num = len(datas)
     msg_list = []
+    b = 0
     for num in datas[len(datas)-fetch_num::]:
+        b+=1
         typ , data = gmail.fetch(num,'(RFC822)')
         raw_email=data[0][1]
         m = BytesParser(policy=policy.default).parsebytes(data[0][1])
         from_ = m.get('From',failobj='')
         dat = m.get('Date')
+        sub = m.get('Subject')
+        fr=""
+        flag = 0
+        for i in range(len(from_)):
+            if from_[i]=='@':
+                flag=1
+            if from_[i]==">":
+                break
+            if flag==1:
+                fr+=from_[i]
+        #msg = email.message_from_string(raw_email.decode('utf-8'))
+        #msg_encoding = email.header.decode_header(msg.get('Subject'))[0][1] or 'iso-2020-jp'
+        #msg_subject = email.header.decode_header(msg.get('Subject'))[0][0]
+        #subject = str(msg_subject.decode(msg_encoding))
+        msg_list.append([b,dat,fr,sub]) 
         
-        msg = email.message_from_string(raw_email.decode('utf-8'))
-        msg_encoding = email.header.decode_header(msg.get('Subject'))[0][1] or 'iso-2020-jp'
-        msg_subject = email.header.decode_header(msg.get('Subject'))[0][0]
-        subject = str(msg_subject.decode(msg_encoding))
-        body = msg.get_payload()
-        msg_list.append([from_,subject]) 
-        a = Mails(name=id,date=dt,domain=from_,title=subject)
-        b = Mails.objects.filter(title=subject).count()
-        if b == 1:
-            break
-        a.save()
-    
     gmail.close()
     gmail.logout() 
+    for i in range(len(msg_list)):
+        
+        
+        a = Mails(no=msg_list[i][0],date=msg_list[i][1],domain=msg_list[i][2],title=msg_list[i][3],now=dt)
+        a.save()
     context={'head':head,'data':data,'list':msg_list}
     return render(request,'gmailservice/list.html',context)
 def list(request):
-    
-    
-    return render(request,'gmailservice/list.html')
+    b = Mails.objects.all().values()
+    a = [[0,"","",""]for i in range(len(b))]
+    for i in range(len(b)):
+        a[i][0]=b[i]['no']
+        a[i][1]=b[i]['date']
+        a[i][2]=b[i]['domain']
+        a[i][3]=b[i]['title']
+    context={'list':a}
+    return render(request,'gmailservice/list.html',context)
 
 def count(request):
     b = Mails.objects.all().values()
-    result = [[0,'']for i in range(len(b))]
+    e = Mails.objects.distinct().values_list('domain')
+    
+    a = []
     for i in range(len(b)):
-        result[i][1]=b[i]['domain']
-        result[i][0]+=1
-    context={'value':result}
+        a.append(b[i]['domain'])
+    
+    
+    c = [[0,""]for i in range(len(e))]
+    for i in range(len(e)):
+            
+        c[i][1]=e[i][0]
+    
+    
+    result = [[0,'']for i in range(len(e))]
+    for i in range(len(b)):
+        for j in range(len(e)):
+            if b[i]['domain']==c[j][1]:
+                c[j][0]+=1
+    c.sort(reverse=True)
+    context={'value':c}
     return render(request,'gmailservice/count.html',context)
